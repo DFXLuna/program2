b7be3e5dcb567dd6df5d678ed43de218015c595e
Matt Grant <teamuba@gmail.com>
2017-09-24 12:42:13 -0600

    Completed

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 # Makefile
 # Date: -1 Dec 1440
-CC=g++
+CXX=g++
 CFLAGS=-ggdb -Wall -std=c++11
 LEX=flex++
 LEXFLAGS=--warn
@@ -8,7 +8,7 @@ LEXFLAGS=--warn
 .PHONY: clean
 
 program2: program2.cpp program2_lex.cpp messenger.cpp messenger.hpp
-	${CC} ${CFLAGS} program2.cpp program2_lex.cpp messenger.cpp -o program2
+	${CXX} ${CFLAGS} program2.cpp program2_lex.cpp messenger.cpp -o program2
 
 program2_lex.cpp: program2.lpp messenger.hpp
 	${LEX} ${LEXFLAGS} program2.lpp
--- program2.cpp
+++ program2.cpp
@@ -16,14 +16,20 @@ int main(int argc, char* argv[]){
     //Flex hooks up to stdio by default
     yyFlexLexer f;
     int ret;
+    // Not sure if this belongs here
+    int errCount = 0;
     
     cout << "Line\tColumn\tToken\tLength\tValue" << endl;
     while((ret = f.yylex()) != END){
         if(ret < 90 && ret > 0){
             messenger->print();
         }
-        else if(ret == -1){
-            return -1;
+        else if(ret == 90){
+            errCount++;
+            if(errCount > 20){
+                cout << "Too many errors" << endl;
+                return -1;
+            }
         }
     }
     
